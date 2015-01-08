b3918201aa14457eb038a536a67fe44587e45faa
xujun.1986927@gmail.com <xujun.1986927@gmail.com>
2015-01-08 11:14:03 +0800

    add Events to front

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ css_dir = "css" # by Compass.app
 sass_dir = "sass" # by Compass.app 
 images_dir = "images" # by Compass.app 
 output_style = :compressed # by Compass.app 
-relative_assets = false # by Compass.app 
+relative_assets = true # by Compass.app 
 line_comments = true # by Compass.app 
-sass_options = {:debug_info=>false} # by Compass.app 
+sass_options = {:debug_info=>true} # by Compass.app 
 sourcemap = false # by Compass.app 
