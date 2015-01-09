10b89ec4e35596b7b965903a1b38fa186488c5fe
xujun.1986927@gmail.com <xujun.1986927@gmail.com>
2015-01-09 14:00:25 +0800

    write the 'sass' style code

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 css_dir = "css" # by Compass.app 
 sass_dir = "sass" # by Compass.app 
 images_dir = "images" # by Compass.app 
-output_style = :compressed # by Compass.app 
-relative_assets = true # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = false # by Compass.app 
 line_comments = true # by Compass.app 
-sass_options = {:debug_info=>true} # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
 sourcemap = false # by Compass.app 
